1431254240a1d395adcbed067188fad630618d7c
Atef Arfaoui <atef@kaptio.com>
2020-11-25 01:58:29 +0100

    Day3

--- day3/day3.go
+++ day3/day3.go
@@ -17,45 +17,35 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		lineStr := scanner.Text()
-		//lineStr = "^>v<"
 		fmt.Println(lineStr)
+		lineStr = ">"
 		i := 10000
 		j := 10000
 		arr[i][j] = 1
 		sum = 1
-		for k, rune := range lineStr {
-			fmt.Println(k, string(rune))
+		for i, rune := range lineStr {
+			fmt.Println(i)
+			if arr[i][j] > 0 {
+				continue
+			}
 			if string(rune) == ">" {
 				j += 1
-				if arr[i][j] > 0 {
-					continue
-				}
 				arr[i][j] = 1
 				sum += 1
 			} else if string(rune) == "^" {
 				i += 1
-				//fmt.Println(k, i, j, arr[i][j])
-				if arr[i][j] > 0 {
-					continue
-				}
 				arr[i][j] = 1
 				sum += 1
 			} else if string(rune) == "<" {
 				j -= 1
-				if arr[i][j] > 0 {
-					continue
-				}
 				arr[i][j] = 1
 				sum += 1
 			} else {
 				i -= 1
-				if arr[i][j] > 0 {
-					continue
-				}
 				arr[i][j] = 1
 				sum += 1
 			}
 		}
 	}
-	fmt.Println("sum: ", sum)
+	fmt.Println(sum)
 }
